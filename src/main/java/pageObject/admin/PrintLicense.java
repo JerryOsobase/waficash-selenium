2339c00eb0b4578eb664c773cd4887ff1e60598e
JerryOsobase <osobasejerry@gmail.com>
2022-03-09 19:40:30 +0100

    print id test

--- src/main/java/pageObject/admin/PrintLicense.java
+++ src/main/java/pageObject/admin/PrintLicense.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PrintLicense {
 
@@ -15,28 +17,51 @@ public WebDriver driver;
 		this.driver=driver;
 	}
 	
+	
 	private By printLicenseHeader= By.xpath("//h3[@class='text-themecolor m-b-0 m-t-0 app-title']"); 
-	private By reprintTab= By.cssSelector("a[href*='reprint']");
+	private By previouslyPrintedTab= By.cssSelector("a[href*='reprint']");
 	private By pendingTab= By.cssSelector("a[href*='pending']"); 
-	private By checkbox= By.cssSelector("//tbody //input");
+	private By checkbox= By.xpath("//tbody //input");
 	private By selectAllCheckbox= By.xpath("//input[@type='checkbox']");
 	private By activePagination = By.xpath("//li[@class='page-item pagination-page-nav active']");
 	private By nextPagination = By.xpath("//li[@class='page-item pagination-page-nav active'] //following-sibling::li");
 	private By status = By.xpath("//tr/td[3]");
+	private By permitId = By.xpath("//tr/td[6]");
 	private By printIdButton = By.xpath("//button[text()=' Print ID ']");
 	private By printBulkButton = By.xpath("//button[text()=' Print Bulk ']");
+	private By searchField = By.xpath("//input[@placeholder='Search using name, email address or LSLB Permit Number...']");
+	private By pageTab= By.cssSelector("ul[class*='customtab'] .nav-item");
+	private By tableHeader = By.xpath("//tr/th");
+	private By emptyApplicationText= By.xpath("//div[@class='text-center']/p");
+	
 	
 	
 	public WebElement getPrintLicenseHeader() {
 		return driver.findElement(printLicenseHeader);
 	}
 	
+	public List<WebElement> getPermitId() {
+		return driver.findElements(permitId);
+	}
+	
+	public List<WebElement> getTableHeader() {
+		return driver.findElements(tableHeader);
+	}
+	
+	public List<WebElement> getPageTab() {
+		return driver.findElements(pageTab);
+	}
+	
+	public WebElement getSearchField() {
+		return driver.findElement(searchField);
+	}
+	
 	public WebElement getPrintBulkButton() {
 		return driver.findElement(printBulkButton);
 	}
 	
-	public WebElement getPrintIdButton() {
-		return driver.findElement(printIdButton);
+	public List<WebElement> getPrintIdButton() {
+		return driver.findElements(printIdButton);
 	}
 	
 	public List<WebElement> getStatus() {
@@ -59,11 +84,21 @@ public WebDriver driver;
 		return driver.findElements(checkbox);
 	}
 	
-	public WebElement getPendingTab() {
-		return driver.findElement(pendingTab);
+	public WebElement getEmptyApplicationText() {
+		return driver.findElement(emptyApplicationText);
 	}
 	
-	public WebElement getReprintTab() {
-		return driver.findElement(reprintTab);
+	public Boolean getPendingTab() {
+		 driver.findElement(pendingTab).click();
+			@SuppressWarnings("deprecation")
+			WebDriverWait wait = new WebDriverWait(driver,10) ;
+			return	wait.until(ExpectedConditions.attributeContains(pendingTab, "class", "active"));
+	}
+	
+	public Boolean getPreviouslyPrintedTab() {
+		 driver.findElement(previouslyPrintedTab).click();
+		@SuppressWarnings("deprecation")
+		WebDriverWait wait = new WebDriverWait(driver,10) ;
+		return	wait.until(ExpectedConditions.attributeContains(previouslyPrintedTab, "class", "active"));
 	}
 }
